948d8f2f4bd2daa844d4f54f69d90ef9c1200bab
atmavi <atmavi0001@gmail.com>
2020-05-16 23:15:46 +0800

    Implemented object destructuring on /routes/campground.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,41 +1,44 @@
-var express		= require('express'),
-	router		= express.Router(),
-	Camp		= require('../models/camp'),
-	middleware	= require('../middleware');
+var express = require('express'),
+	router = express.Router(),
+	Camp = require('../models/camp'),
+	middleware = require('../middleware');
 
 //INDEX
-router.get("/", function(req, res){
-	Camp.find({}, function(err, allCamps){
-		if(err){
+router.get("/", function (req, res) {
+	Camp.find({}, function (err, allCamps) {
+		if (err) {
 			console.log(err);
-		}else{
-			res.render("camps/index", {camps:allCamps});
+		} else {
+			res.json(allCamps);
 		}
 	});
-	
+
 });
 
 //NEW
-router.get("/new", middleware.isLoggedIn, function(req, res){
+router.get("/new", middleware.isLoggedIn, function (req, res) {
 	res.render("camps/new");
 });
 
 //CREATE
-router.post("/", middleware.isLoggedIn, function(req, res){
-	var name=req.body.name;
-	var price= req.body.price;
-	var image=req.body.image;
-	var description=req.body.description;
-	var author={
-		id:req.user._id,
-		username:req.user.username
+router.post("/", middleware.isLoggedIn, function (req, res) {
+	let { name, price, image, description } = req.body;
+	// var name = req.body.name;
+	// var price = req.body.price;
+	// var image = req.body.image;
+	// var description = req.body.description;
+
+	let { _id, username } = req.user;
+	var author = {
+		id: _id,
+		username: username
 	};
-	                            
-	var newCamp={name: name,price: price, img:image, desc:description, author:author};
-		Camp.create(newCamp, function(err, camp){
-		if(err){
+
+	var newCamp = { name: name, price: price, img: image, desc: description, author: author };
+	Camp.create(newCamp, function (err, camp) {
+		if (err) {
 			console.log(err);
-		}else{
+		} else {
 			req.flash("success", "Camp successfully created.");
 			res.redirect("/index");
 		}
@@ -43,51 +46,51 @@ router.post("/", middleware.isLoggedIn, function(req, res){
 });
 
 //SHOW
-router.get("/:id",function(req,res){
-	var id=req.params.id;
-	Camp.findById(id).populate("comments").exec(function(err, foundCamp){
-		if(err){
+router.get("/:id", function (req, res) {
+	var id = req.params.id;
+	Camp.findById(id).populate("comments").exec(function (err, foundCamp) {
+		if (err) {
 			console.log(err);
-		}else{
-			res.render("camps/show", {fCamp:foundCamp});
+		} else {
+			res.render(foundCamp);
 		}
 	});
 });
 
 //EDIT
-router.get("/:id/edit", middleware.checkCampOwnership, (req, res)=>{
-	Camp.findById(req.params.id, (err, fCamp)=>{
-		if(err){
+router.get("/:id/edit", middleware.checkCampOwnership, (req, res) => {
+	Camp.findById(req.params.id, (err, fCamp) => {
+		if (err) {
 			res.render("index");
-		}else{
-			res.render("camps/edit", {fCamp:fCamp});
+		} else {
+			res.render("camps/edit", { fCamp: fCamp });
 		}
 	});
 });
 
 //UPDATE
-router.put("/:id", (req, res)=>{
-	Camp.findByIdAndUpdate(req.params.id, req.body.camp, (err, camp)=>{
+router.put("/:id", (req, res) => {
+	Camp.findByIdAndUpdate(req.params.id, req.body.camp, (err, camp) => {
 		console.log(camp);
-		if(err){
+		if (err) {
 			res.render("index");
-		}else{
+		} else {
 			req.flash("success", "Camp successfully updated.");
-			res.redirect("/index/"+req.params.id);
+			res.redirect("/index/" + req.params.id);
 		}
 	});
 });
 
 //DELETE
-router.delete("/:id", middleware.checkCampOwnership, (req, res)=>{
-	Camp.findByIdAndRemove(req.params.id, (err)=>{
-		if(err){
+router.delete("/:id", middleware.checkCampOwnership, (req, res) => {
+	Camp.findByIdAndRemove(req.params.id, (err) => {
+		if (err) {
 			res.render("index");
-		}else{
+		} else {
 			req.flash("success", "Camp successfully deleted.");
 			res.redirect("/index");
 		}
 	});
 });
 
-module.exports= router;
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
-var express	= require('express'),
-	router	= express.Router(),
-	passport= require('passport');
-	User	= require('../models/users');
+var express = require('express'),
+	router = express.Router(),
+	passport = require('passport');
+User = require('../models/users');
 
 //ROUTES
-router.get("/", function(req, res){
-	res.render("landing");
+router.get("/", function (req, res) {
+	res.redirect('/index')
 });
 
-router.get('/register', (req, res)=>{
+router.get('/register', (req, res) => {
 	res.render('users/register');
 });
 
-router.post('/register', (req, res)=>{
-	User.register(new User({username:req.body.username}), req.body.password, (err, user)=>{
-		if(err){
+router.post('/register', (req, res) => {
+	User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+		if (err) {
 			req.flash("error", err.message);
 			return res.redirect("/register");
 		}
@@ -24,21 +24,21 @@ router.post('/register', (req, res)=>{
 });
 
 //SHOW LOGIN
-router.get('/login', (req, res)=>{
+router.get('/login', (req, res) => {
 	res.render('login');
 });
 
 router.post('/login', passport.authenticate("local", {
-	successRedirect:"/index",
-	failureRedirect:"/login"
-}), (req, res)=>{
+	successRedirect: "/index",
+	failureRedirect: "/login"
+}), (req, res) => {
 });
 
-router.get('/logout',(req, res)=>{
+router.get('/logout', (req, res) => {
 	req.logout();
 	req.flash("success", "You have been logged out.");
 	console.log(req.flash.success);
 	res.redirect('/index');
 });
 
-module.exports= router;
+module.exports = router;
